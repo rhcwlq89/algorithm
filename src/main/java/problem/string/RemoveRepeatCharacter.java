2673200782fc099d6799a68d20054ee068251e1f
이호종 <ccaja21@naver.com>
2023-01-06 17:29:49 +0900

    코테 문제 추가

--- src/main/java/problem/string/RemoveRepeatCharacter.java
+++ src/main/java/problem/string/RemoveRepeatCharacter.java
@@ -1,10 +1,33 @@
 package problem.string;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RemoveRepeatCharacter {
 
     public String[] solution(String s) {
-        String[] answer = {"pi", "a"};
+        // only lowercase
+        String pattern = "(.)\\1*";
+        List<String> list = new ArrayList<>();
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(s);
+        StringBuilder builder = new StringBuilder();
 
-        return answer;
+        while (m.find()) {
+            String sub = m.group();
+            if(sub.length() <= 1) {
+                builder.append(sub);
+            } else {
+                list.add(builder.toString());
+                builder = new StringBuilder();
+            }
+        }
+
+        list.add(builder.toString());
+
+        String[] a = new String[list.size()];
+        return list.toArray(a);
     }
 }
--- src/test/java/problem/string/RemoveRepeatCharacterTest.java
+++ src/test/java/problem/string/RemoveRepeatCharacterTest.java
@@ -6,14 +6,50 @@ import org.junit.jupiter.api.Test;
 public class RemoveRepeatCharacterTest {
 
     @Test
-    public void removeRepeatCharacterTest() {
+    public void removeRepeatCharacterTest1() {
         RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
         String[] pizzas = repeatCharacter.solution("pizza");
         String[] expected1 = {"pi", "a"};
         Assertions.assertArrayEquals(expected1, pizzas);
+    }
 
+    @Test
+    public void removeRepeatCharacterTest2() {
+        RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
         String[] mississippis = repeatCharacter.solution("mississippi");
         String[] expected2 = {"mi", "i", "i", "i"};
         Assertions.assertArrayEquals(expected2, mississippis);
+    }
+
+    @Test
+    public void removeRepeatCharacterTest3() {
+        RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
+        String[] aabbbbs = repeatCharacter.solution("aabbbb");
+        String[] expected3 = {"", ""};
+        Assertions.assertArrayEquals(expected3, aabbbbs);
+    }
+
+    @Test
+    public void removeRepeatCharacterTest4() {
+        RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
+        String[] aabcddddefgggs = repeatCharacter.solution("aabcddddefggg");
+        String[] expected = {"","bc","ef",""};
+        Assertions.assertArrayEquals(expected, aabcddddefgggs);
+    }
+
+    @Test
+    public void removeRepeatCharacterTest5() {
+        RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
+        String[] abyyys = repeatCharacter.solution("abyyy");
+        String[] expected = {"ab", ""};
+        Assertions.assertArrayEquals(expected, abyyys);
+    }
+
+    @Test
+    public void removeRepeatCharacterTest6() {
+        RemoveRepeatCharacter repeatCharacter = new RemoveRepeatCharacter();
+        String[] kkkkkkks = repeatCharacter.solution("kkkkkkk");
+        String[] expected = {"", ""};
+        Assertions.assertArrayEquals(expected, kkkkkkks);
     }
 }
