f00d808d63d6eb12c9a51fbe79daffd50352e843
이호종 <ccaja21@naver.com>
2023-01-16 16:06:44 +0900

    catchMole

--- src/main/java/problem/search/binary/breadth/CatchMole.java
+++ src/main/java/problem/search/binary/breadth/CatchMole.java
@@ -1,10 +1,86 @@
 package problem.search.binary.breadth;
 
+import java.util.*;
+
 public class CatchMole {
 
     public int solution(int n, int m, int[][] moles) {
+        List<Mole> moleList = new ArrayList<>();
+        for (int[] mole : moles) {
+            Mole mole1 = new Mole(mole[0], mole[1], mole[2]);
+            moleList.add(mole1);
+        }
+        moleList.sort(Comparator.comparingInt(Mole::getTime));
 
+        int maxScore = 0;
+        for(int i = 1; i <= n; i++) {
+            Set<Integer> set = new HashSet<>();
+            set.add(i);
 
-        return 0;
+            for(int j = 1; j < m; j++) {
+                int e = (i + j);
+                set.add(e > n? e % n : e);
+            }
+
+            Stack<Mole> stack = new Stack<>();
+            for (Mole mole : moleList) {
+                if (set.contains(mole.getNumber())) {
+                    if(stack.isEmpty()) stack.push(mole);
+                    else {
+                        Mole peek = stack.peek();
+                        if(peek.getTime() == mole.getTime() && peek.getScore() <= mole.getScore()) {
+                            stack.pop();
+                            stack.push(mole);
+                        } else {
+                            stack.push(mole);
+                        }
+                    }
+
+                }
+            }
+
+            int scoreSum = 0;
+            while(!stack.isEmpty()) {
+                Mole pop = stack.pop();
+                scoreSum += pop.getScore();
+            }
+
+            if(scoreSum > maxScore) maxScore = scoreSum;
+        }
+
+        return maxScore;
+    }
+
+    public static class Mole {
+        private int time;
+        private int number;
+        private int score;
+
+        public Mole(int time, int number, int score) {
+            this.time = time;
+            this.number = number;
+            this.score = score;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        @Override
+        public String toString() {
+            return "Mole{" +
+                    "time=" + time +
+                    ", number=" + number +
+                    ", score=" + score +
+                    '}';
+        }
     }
 }
--- src/test/java/problem/search/breadth/CatchMoleTest.java
+++ src/test/java/problem/search/breadth/CatchMoleTest.java
@@ -10,7 +10,16 @@ public class CatchMoleTest {
     @Test
     public void testCatchMole() {
         CatchMole catchMole = new CatchMole();
-        int[][] moles = {{1, 1, 3}, {1, 4, 5}, {1, 5 ,6}, {1, 8, 4}, {3, 1, 2}, {4, 6, 1}, {7, 2, 2}, {7, 8, 2}};
+        int[][] moles = {
+                {1, 1, 3},
+                {1, 4, 5},
+                {1, 5 ,6},
+                {1, 8, 4},
+                {3, 1, 2},
+                {4, 6, 1},
+                {7, 2, 2},
+                {7, 8, 2}
+        };
 
         int solution = catchMole.solution(8, 3, moles);
         assertEquals(8, solution);
@@ -19,7 +28,16 @@ public class CatchMoleTest {
     @Test
     public void testCatchMole2() {
         CatchMole catchMole = new CatchMole();
-        int[][] moles = {{7, 5, 6}, {2, 1, 10}, {6, 6 ,6}, {2, 2, 10}, {3, 2, 10}, {5, 5, 6}, {8, 6, 6}, {3, 1, 10}};
+        int[][] moles = {
+                {7, 5, 6},
+                {2, 1, 10},
+                {6, 6 ,6},
+                {2, 2, 10},
+                {3, 2, 10},
+                {5, 5, 6},
+                {8, 6, 6},
+                {3, 1, 10}
+        };
 
         int solution = catchMole.solution(7, 2, moles);
         assertEquals(24, solution);
